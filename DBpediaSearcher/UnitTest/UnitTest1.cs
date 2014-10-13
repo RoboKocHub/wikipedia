b718f6b7b04cdfd11c12ae3e52195e5a41d21751
Robert Kocian <robert.kocian@gmail.com>
2014-10-13 10:44:54 +0200

    change unit test method

--- DBpediaSearcher/UnitTest/UnitTest1.cs
+++ DBpediaSearcher/UnitTest/UnitTest1.cs
@@ -10,7 +10,7 @@ namespace UnitTest
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ParseFileTest()
         {
             var fo = new FileOperation();
             var output = fo.ParseToXml(@"(?<http>\w+):\/\/(?<dbpedia>[\w@][\w.:@]+)\/?[\w\.?=%&=\-@/$,]*", @"C:\D\Skola\STUBA\2roc\VI\Project\\DBpediaSearcher\Files\sample.txt");
