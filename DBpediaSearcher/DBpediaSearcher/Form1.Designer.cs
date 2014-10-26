22bd5d98a8376d69c36809f87581d1f69ce991c9
Robert Kocian <robert.kocian@gmail.com>
2014-10-26 21:06:13 +0100

    created samples data and parsing disambiguationLink
    Included disambiguation and pagelink sample
    Created c# code for parsing and disambiguation links

---
+++ DBpediaSearcher/DBpediaSearcher/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace DBpediaSearcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.fileName = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 84);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Štart";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // fileName
+            // 
+            this.fileName.Location = new System.Drawing.Point(12, 34);
+            this.fileName.Name = "fileName";
+            this.fileName.Size = new System.Drawing.Size(100, 20);
+            this.fileName.TabIndex = 1;
+            this.fileName.Text = "sample.txt";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(139, 34);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(677, 487);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(828, 539);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.fileName);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox fileName;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ DBpediaSearcher/Infrastructure/Model/RelatedPageLinkModel/RelatedPageLink.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Infrastructure.Model
+{
+    public class RelatedPageLink
+    {
+        public List<Article> ArticleList { get; set; }
+
+    }
+
+    //public class Category {
+    //    public string Name {get; set;}
+       
+    //}
+
+    public class Article {
+        public string Name { get; set; }
+        public List<Link> LinkList { get; set; }
+    }
+
+    public class Link {
+        public string LinkType { get; set; }
+        public string LinkName { get; set; }
+        public List<DisambiguationLink> DisambiguationLinkList { get; set; }
+    }
+
+
+
+    public class DisambiguationLink {
+        public string Name {get;set;}
+        public string Type {get;set;}
+
+    }
+
+}
