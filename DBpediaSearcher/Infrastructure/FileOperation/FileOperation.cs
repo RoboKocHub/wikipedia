f4388c8731d4559248d7c265226fa95f01b9bec7
Robert Kocian <robert.kocian@gmail.com>
2014-12-18 23:00:48 +0100

    update small samples and infrastructure
    I use en dbpedia data but this eu sample is small that is why It is in
    branch. Update cumputing and parsing

--- DBpediaSearcher/Infrastructure/FileOperation/FileOperation.cs
+++ DBpediaSearcher/Infrastructure/FileOperation/FileOperation.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Infrastructure.Model;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -13,6 +14,22 @@ namespace Infrastructure
 {
    public class FileOperation
     {
+       public List<string> DisambiguationLinks { get; set; }
+       public string DisambiguationFilePath = @"C:\D\Skola\STUBA\2roc\VI\Project\DBpediaSearcher\Files\disambiguations_en.ttl";
+       //public string DisambiguationFilePath = @"C:\D\Skola\STUBA\2roc\VI\Project\DBpediaSearcher\Files\disambiguations_en_uris_eu.ttl";
+       public string ShortFilePath = @"C:\D\Skola\STUBA\2roc\VI\Project\DBpediaSearcher\Files\short_abstracts_en.ttl";
+       public List<string> ShortAbstracts { get; set; }
+       public int SuccessRecommendedDisLink {get;set;}
+       public int NotSuccessRecommendedDisLink { get; set; }
+       public int ArticleCounter { get; set; }
+       public int NotFindDisambiguationLink { get; set; }
+
+       public FileOperation() {
+           var lines = File.ReadLines(DisambiguationFilePath).ToList();
+           DisambiguationLinks = lines;
+           ShortAbstracts = File.ReadAllLines(ShortFilePath).ToList();
+       }
+
        //public Model.RelatedPageLink Parse(string regex, string path, bool onlyDisambiguationLink)
        //{
        //    Model.RelatedPageLink rpl = new Model.RelatedPageLink();
@@ -65,22 +82,26 @@ namespace Infrastructure
        //    return rpl;
        //}
 
-       public string DisambiguationFilePath = @"C:\D\Skola\STUBA\2roc\VI\Project\DBpediaSearcher\Files\disambiguation.txt";
-
+       /// <summary>
+        /// parsovanie liniek v clanku
+       /// </summary>
+       /// <param name="regex"></param>
+       /// <param name="path"></param>
+       /// <param name="onlyDisambiguationLink"></param>
+       /// <returns></returns>
        public string ParseToXml(string regex, string path, bool onlyDisambiguationLink)
        {
            Model.RelatedPageLink rpl = new Model.RelatedPageLink();
-           rpl.ArticleList = new List<Model.Article>();
+           rpl.LinkList = new List<Model.Link>();
            XmlDocument xmlDoc = new XmlDocument();
            // Pattern to check each line
            Regex pattern = new Regex(regex);
 
-           // Read in lines
-           string[] lines = File.ReadAllLines(path);
-
-
+           System.IO.StreamReader file = new System.IO.StreamReader(path);
+           string result = "";
            // Iterate through lines
-           foreach (string line in lines)
+           string line;
+           while ((line = file.ReadLine()) != null)
            {
                // Check if line matches your format here
                var matches = pattern.Matches(line);
@@ -89,42 +110,224 @@ namespace Infrastructure
 
                if (matches.Count > 2 && matches[2].Groups[0].Value.Contains("_(disambiguation)"))
                {
-                   Model.Article rplArticle =
-                           new Model.Article
-                           {
-                               Name = matches[0].Groups[0].Value,
-                               LinkList = new List<Model.Link>() {
-                                   new Model.Link() {
-                                      LinkType = matches[1].Groups[0].Value,
-                                      LinkName = matches[2].Groups[0].Value,
-                                      DisambiguationLinkList = GetLinksOfDisambiguetionPage(matches[2].Groups[0].Value,regex)
-                                   }
-                                }
-                           };
+                   Console.WriteLine("--------------------------------------------------------------");
+                   var articleKeywords = GetShortAbstract(matches[0].Groups[0].Value).ToList();
+                   var link = new Model.Link()
+                                   {
+                                       ArticleName = matches[0].Groups[0].Value,
+                                       //ArticleAbstract = GetShortAbstract(matches[0].Groups[0].Value),
+                                       LinkType = matches[1].Groups[0].Value,
+                                       LinkName = matches[2].Groups[0].Value,
+                                       Keywords = articleKeywords, //ParseKeyWords(matches[2].Groups[0].Value),
+                                       DisambiguationLinkList = GetLinksOfDisambiguetionPage(matches[2].Groups[0].Value, regex, articleKeywords),
+                                      
+                                   };
+                   rpl.LinkList.Add(link);
+                   ArticleCounter += 1;
+                   
+                   link.RecomendedDisambiguationLink = GetPrediction(link.DisambiguationLinkList);
+                   if (link.RecomendedDisambiguationLink.Count >= 1 && link.RecomendedDisambiguationLink.First().DisambiguationValue != 0)
+                   {
+                       SuccessRecommendedDisLink += 1;
+                       Console.WriteLine(string.Format("<ENDLINK number:{0}  link name: {1}, recomended link: {2} ENDLINK>", ArticleCounter, link.LinkName, link.RecomendedDisambiguationLink.First().DisambiguationValue));
+                   }
+                   else
+                   {
+                       NotSuccessRecommendedDisLink += 1;
+                       Console.WriteLine(string.Format("<ENDLINK number:{0}  link name: {1}, recomended link: {2} ENDLINK>", ArticleCounter, link.LinkName, 0));
+                   }
+                   
+                  
+                   if (ArticleCounter > 50) {
+                       file.Close();
+                       break;
 
-                   rpl.ArticleList.Add(rplArticle);
-
-                   Console.WriteLine(string.Format("{0}", rpl));
+                   }
                }
 
            }
+           //var tt = rpl.LinkList.Where(w => w.RecomendedDisambiguationLink.First().DisambiguationValue != 0);
+           //if (tt.Count() > 1)
+           //{
+               XmlSerializer xmlSerializer = new XmlSerializer(typeof(Model.RelatedPageLink));
+               using (MemoryStream xmlStream = new MemoryStream())
+               {
 
-           XmlSerializer xmlSerializer = new XmlSerializer(typeof(Model.RelatedPageLink));
-           using (MemoryStream xmlStream = new MemoryStream())
-           {
-               xmlSerializer.Serialize(xmlStream, rpl);
-               xmlStream.Position = 0;
-               xmlDoc.Load(xmlStream);
-               //return xmlDoc.InnerXml;
-           }
+                   xmlSerializer.Serialize(xmlStream, rpl);
+                   xmlStream.Position = 0;
+                   xmlDoc.Load(xmlStream);
+                   //return xmlDoc.InnerXml;
+               }
 
-           var stringWriter = new StringWriter(new StringBuilder());
-           var xmlTextWriter = new XmlTextWriter(stringWriter) { Formatting = Formatting.Indented };
-           xmlDoc.Save(xmlTextWriter);
-           return stringWriter.ToString();
+               var stringWriter = new StringWriter(new StringBuilder());
+               var xmlTextWriter = new XmlTextWriter(stringWriter) { Formatting = Formatting.Indented };
+               xmlDoc.Save(xmlTextWriter);
+               return stringWriter.ToString();
+          // }
+           //return null;
 
+         
            //return null;
        }
+
+
+
+       
+       
+       /// <summary>
+       /// parsovanie disambiguacnych liniek
+       /// </summary>
+       /// <param name="linkname"></param>
+       /// <param name="regex"></param>
+       /// <param name="articleKeywords"></param>
+       /// <returns></returns>
+        public List<Infrastructure.Model.DisambiguationLink> GetLinksOfDisambiguetionPage(string linkname, string regex, List<string> articleKeywords){
+            Console.WriteLine(string.Format("START GetLinksOfDisambiguetionPage for linkname: {0}", linkname));
+            
+            List<Model.DisambiguationLink> dsgList = new List<Model.DisambiguationLink>();
+            XmlDocument xmlDoc = new XmlDocument();
+            // Pattern to check each line
+            Regex pattern = new Regex(regex);
+
+            System.IO.StreamReader file = new System.IO.StreamReader(DisambiguationFilePath);
+            string result = "";
+            // Iterate through lines
+            IEnumerable<string> lines = DisambiguationLinks.FindAll(a => a.Contains(linkname));
+            foreach (string line in lines)
+            {
+                // Check if line matches your format here
+                var matches = pattern.Matches(line);
+                if (matches.Count > 2 && matches[0].Groups[0].Value.Contains(linkname))
+                {
+                    var dsg = new Model.DisambiguationLink();
+                    dsg.Type = matches[0].Groups[0].Value;
+                    dsg.Name = matches[2].Groups[0].Value;
+                    dsg.Keywords = GetShortAbstract(matches[2].Groups[0].Value).ToList(); //ParseKeyWords(matches[2].Groups[0].Value);
+                    dsg.ProbabilityRecommendation = GetprobabilityOfOcurrenceWords(articleKeywords, dsg.Keywords);
+                    //dsg.DisambiguationAbstract = GetShortAbstract(matches[2].Groups[0].Value);
+                    dsgList.Add(dsg);
+                    // }
+                }
+            }
+            Console.WriteLine(string.Format("END GetLinksOfDisambiguetionPage CountDISLinks: {0}", dsgList.Count()));
+            return dsgList;
+            
+        }
+
+
+
+       /// <summary>
+       /// parsovat skrateny abstrakt
+       /// </summary>
+       /// <param name="linkname"></param>
+       /// <returns></returns>
+        public IEnumerable<string> GetShortAbstract(string linkname) {
+            Console.WriteLine(string.Format("START GetShortAbstract for linkname: {0}", linkname));
+              // Pattern to check each line
+            Regex patternToFindID = new Regex(@"(?<http>\w+):\/\/(?<dbpedia>[\w@][\w.:@]+)\/?[\w\.?()_=%&=\-@/$,]*");
+            //Regex patternToFindTextInQuotation = new Regex(@"([^"]*)")
+             
+            string result = "";
+            // Iterate through lines
+          
+            //const Int32 BufferSize = 1024;
+            //using (var fileStream = File.OpenRead(ShortFilePath))
+            //using (var streamReader = new StreamReader(fileStream, Encoding.UTF8, true, BufferSize))
+            //{
+            //    String line;
+                //while ((line = streamReader.ReadLine()) != null)
+
+            string line = ShortAbstracts.Find(a => a.Contains(linkname));
+
+            //foreach(var line in ShortAbstracts )
+            //    {
+            //        // Process line
+            if (!string.IsNullOrEmpty(line))
+            {
+                var matches = patternToFindID.Matches(line);
+                if (matches.Count == 2 && matches[0].Groups[0].Value.Contains(linkname))
+                {
+                    Regex patternToFindAbstract = new Regex(@"!(?<http>\w+):\/\/(?<dbpedia>[\w@][\w.:@]+)\/?[\w\.?()_=%&=\-@/$,]*");
+                    string[] stringSeparators = new string[] { "rdf-schema#comment>" };
+                    result = line.Split(stringSeparators, StringSplitOptions.RemoveEmptyEntries)[1];
+                    //break;
+                }
+                // }
+                //}
+                //Console.WriteLine(string.Format("keywords: {0}", result.Length));
+                var source = result.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                var matchQuery = from word in source
+                                 where word.Length >= 4 && word != "\"" && !word.Contains("lly") && !word.Contains("ed") && word != "Such"
+                                 select word;
+                Console.WriteLine(string.Format("END GetShortAbstract CountAstracts: {0}", matchQuery.Count()));
+                return matchQuery;
+            }
+            else {
+                Console.WriteLine(string.Format("END GetShortAbstract: NoLineFound!!!"));
+                return new List<string>();
+            }
+        }
+
+
+        #region Prediction
+
+        public List<PredictionResult> GetPrediction(List<DisambiguationLink> dl)
+        {
+            var result = new List<PredictionResult>();
+            if (dl.Count() >= 1)
+            {
+                decimal resultvalue = dl.Max(m => m.ProbabilityRecommendation);
+                var resultDLObject = dl.Find(f => f.ProbabilityRecommendation == resultvalue);
+
+                result.Add(new PredictionResult { DisambiguationValue = resultDLObject.ProbabilityRecommendation, DisambiguationName = resultDLObject.Name });
+            }
+            else {
+                Console.Write("NOT FOUND DISAMBIGUATION LINK");
+                NotFindDisambiguationLink += 1;
+            }
+            return result;
+        }
+
+        #endregion
+
+      
+
+
+        #region Additional methods
+
+        public List<string> ParseKeyWords(string name) {
+
+           var arrayName = name.Split('/');
+            var resultList = new List<string>();
+           var result = arrayName[arrayName.Length - 1];
+
+            if(result.Contains("_")) {
+                if (result.Contains("disambiguation"))
+                    resultList.Add(result.Split('_')[0]);
+                else
+                    resultList = result.Split('_').ToList();
+            }
+            else { resultList.Add(result.Split('_')[0]); } 
+        
+           return resultList;
+               
+        }
+
+       public decimal GetprobabilityOfOcurrenceWords(List<string> ArticleKeywords,List<string> DisambiguationKeywords){
+           int counter = 0;
+           foreach(var ak in ArticleKeywords){
+               foreach (var dk in DisambiguationKeywords) {
+                   if (dk.Contains(ak)) {
+                       counter++;
+                   }
+               }
+           }
+
+           return counter;
+               //(counter != 0 ) ? counter/DisambiguationKeywords.Count() : 0;
+       }
+
+    
 
         public string ToXML()
         {
@@ -142,35 +345,8 @@ namespace Infrastructure
         }
            
 
-        public List<Infrastructure.Model.DisambiguationLink> GetLinksOfDisambiguetionPage(string linkname, string regex){
-            List<Model.DisambiguationLink> dsgList = new List<Model.DisambiguationLink>();
-            
-            XmlDocument xmlDoc = new XmlDocument();
-            // Pattern to check each line
-            Regex pattern = new Regex(regex);
+        #endregion
 
-            // Read in lines
-            string[] lines = File.ReadAllLines(DisambiguationFilePath);
-            
-            // Iterate through lines
-            foreach (string line in lines)
-            {
-                // Check if line matches your format here
-                var matches = pattern.Matches(line);
-                if (matches.Count > 2 && matches[0].Groups[0].Value.Contains(linkname))
-                {
-                    var dsg = new Model.DisambiguationLink();
-                    dsg.Type = matches[0].Groups[0].Value;
-                    dsg.Name = matches[2].Groups[0].Value;
-
-                    dsgList.Add(dsg);
-
-                }
-            }
-
-            return dsgList;
-
-        }
-        }
+    }
     }
 
--- DBpediaSearcher/Infrastructure/Model/RelatedPageLinkModel/RelatedPageLink.cs
+++ DBpediaSearcher/Infrastructure/Model/RelatedPageLinkModel/RelatedPageLink.cs
@@ -7,7 +7,7 @@ namespace Infrastructure.Model
 {
     public class RelatedPageLink
     {
-        public List<Article> ArticleList { get; set; }
+        public List<Link> LinkList { get; set; }
 
     }
 
@@ -16,23 +16,44 @@ namespace Infrastructure.Model
        
     //}
 
-    public class Article {
-        public string Name { get; set; }
-        public List<Link> LinkList { get; set; }
-    }
+    //public class Article {
+    //    public string Name { get; set; }
+    //    public List<Link> LinkList { get; set; }
+
+    //}
 
     public class Link {
+        public string ArticleName { get; set; }
+        public string ArticleAbstract { get; set; }
         public string LinkType { get; set; }
         public string LinkName { get; set; }
+        public List<PredictionResult> RecomendedDisambiguationLink { get; set; }
+        public List<string> Keywords { get; set; }
         public List<DisambiguationLink> DisambiguationLinkList { get; set; }
+        public int NumberOfDisambiguationLink { get; set; }
+        
     }
 
 
 
     public class DisambiguationLink {
+        public string DisambiguationAbstract { get; set; }
         public string Name {get;set;}
         public string Type {get;set;}
+        public List<string> Keywords { get; set; }
+        public int NumberOfLink { get; set; }
+        public decimal ProbabilityRecommendation { get; set; }
 
     }
+
+    public class PredictionResult
+    {
+        public string DisambiguationName { get; set; }
+        public decimal DisambiguationValue { get; set; }
+    }
+    //public class KeyWords {
+    //    public string Name { get; set; }
+
+    //}
 
 }
